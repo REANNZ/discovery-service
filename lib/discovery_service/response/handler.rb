d925c2e728bb88c2e54efef7ca66173eb4eb32d2
Bradley Beddoes <bradleybeddoes@gmail.com>
2019-05-29 15:15:38 +1000

    Rubcop satisfaction formating

--- lib/discovery_service/response/handler.rb
+++ lib/discovery_service/response/handler.rb
@@ -19,30 +19,37 @@ module DiscoveryService
         if return_url&.present?
           if DiscoveryService.configuration[:environment][:restrict_return_url]
             if valid_return_url(params, return_url)
-              logger.info("Return URL provided by '#{params[:entityID]}' was valid")
+              logger.info(''"Return URL provided for
+                          '#{params[:entityID]}' was valid"'')
               redirect_to(return_url, params)
             else
-              logger.error("Return URL '#{return_url}' provided by '#{params[:entityID]}' was invalid, rejecting value")
+              logger.error(''"Return URL '#{return_url}' provided for
+                           '#{params[:entityID]}' was invalid,
+                           rejecting value"'')
               redirect to('/error/invalid_return_url')
             end
           else
             if valid_return_url(params, return_url)
-              logger.info("Return URL '#{return_url}' provided by '#{params[:entityID]}' was valid (config disabled)")
+              logger.info(''"Return URL '#{return_url}' provided for
+                          '#{params[:entityID]}' was valid (config disabled)"'')
             else
-              logger.error("Return URL '#{return_url}' provided by '#{params[:entityID]}' was invalid, would be rejected (config disabled)")
+              logger.error(''"Return URL '#{return_url}'
+                           provided for '#{params[:entityID]}' was invalid,
+                           would be rejected (config disabled)"'')
             end
             redirect_to(return_url, params)
           end
         else
-          logger.info("Return URL not provided by '#{params[:entityID]}', fallback to default")
+          logger.info(''"Return URL not provided in
+                      query for '#{params[:entityID]}'"'')
           return_url = default_discovery_response(params)
           return redirect_to(return_url, params) if return_url
 
-          logger.info("Default return URL for '#{params[:entityID]}', not found")
+          logger.info("No default return URL for '#{params[:entityID]}'")
           status 404
         end
       end
-      # rubocop:enable Metrics/LineLength, Metrics/MethodLength, Metrics/AbcSize, Metrics/PerceivedComplexity
+      # rubocop:enable
 
       private
 
